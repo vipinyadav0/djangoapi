f5ae30927081a45fb0bf004b0215c54379c77c46
Vipin Yadav <vyvipinyadav998@gmail.com>
2023-01-04 19:01:57 +0530

    initial commit

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://djangoapi.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
