57295f213c78d2c79a8716a3738f40b6deda84ae
Vipin Yadav <vyvipinyadav998@gmail.com>
2023-01-05 23:19:14 +0530

    views modified

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 
 from django.urls import path, include
-from .views import Students, TeacherViewSets, StudentViewSets
+from .views import Students, TeacherViewSets, StudentViewSets, StudentsList, StudentDetail
 from rest_framework import routers
-
 router = routers.DefaultRouter()
 router.register(r'teachers', TeacherViewSets, basename='teacher')
 router.register(r'students', StudentViewSets, basename='students')
@@ -10,5 +9,8 @@ router.register(r'students', StudentViewSets, basename='students')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('students/', Students.as_view())
+    path('all-students/', Students.as_view()),
+    path('list-students/', StudentsList.as_view()),
+    path('list-students/<int:pk>/', StudentDetail.as_view()),
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,14 +1,58 @@
-from django.shortcuts import render, get_object_or_404
-from .serializers import StudentSerializer, TeachersSerializer
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from rest_framework import status, viewsets
 from rest_framework.generics import GenericAPIView, ListAPIView
-from .models import Student, Teacher
-from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Student, Teacher
+from .serializers import StudentSerializer, TeachersSerializer
+
 # Create your views here.
 
 class Students(ListAPIView):
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
+
+class StudentsList(APIView):
+    def get(self, request):
+        students =  Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StudentDetail(APIView):
+    """Retrieve, Update, Delete a student instance
+
+    """
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class StudentViewSets(viewsets.ViewSet):
     """
